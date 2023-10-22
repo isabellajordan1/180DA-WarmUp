52792c2167773c3628d0cddec796f4141a6306b2
isabellajordan1 <isabella.jordan014@gmail.com>
2023-10-22 16:06:50 -0700

    updates

--- lab 3/com/clientTest.py
+++ lab 3/com/clientTest.py
@@ -2,7 +2,7 @@
 
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('192.168.1.199', 8080))
+client.connect(('169.232.86.218', 8080))
 client.sendall('I am CLIENT\n'.encode())
 from_server = client.recv(4096)
 client.close()
--- lab 3/com/pub.py
+++ lab 3/com/pub.py
@@ -22,7 +22,33 @@ def on_message(client, userdata, message):
     print('Received message: "' + str(message.payload) + '" on topic "' +
     message.topic + '" with QoS ' + str(message.qos))
 
+
 # 1. create a client instance.
+import paho.mqtt.client as mqtt
+import numpy as np
+
+# 0. define callbacks - functions that run when events happen.
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connection returned result: "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    # client.subscribe("ece180d/test")
+    # The callback of the client when it disconnects.
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print('Unexpected Disconnect')
+    else:
+        print('Expected Disconnect')
+# The default message callback.
+# (won’t be used if only publishing, but can still exist)
+def on_message(client, userdata, message):
+    print('Received message: "' + str(message.payload) + '" on topic "' +
+    message.topic + '" with QoS ' + str(message.qos))
+
+
 client = mqtt.Client()
 # add additional client options (security, certifications, etc.)
 # many default options should be good to start off.
