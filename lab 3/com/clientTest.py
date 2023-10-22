c7d811bda475e16301f9bf14728f23be54aed841
isabellajordan1 <isabella.jordan014@gmail.com>
2023-10-22 16:01:49 -0700

    added folder hierarchy and added lab 3 code and pngs

---
+++ lab 3/com/clientTest.py
@@ -0,0 +1,9 @@
+# RPI
+
+import socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('192.168.1.199', 8080))
+client.sendall('I am CLIENT\n'.encode())
+from_server = client.recv(4096)
+client.close()
+print(from_server)
